7f82a2b6e07738ddc78b3261be746d515094781e
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-14 16:16:35 +0530

    implemented and working with check box without adding to database

--- admin/lib/screens/add_product.dart
+++ admin/lib/screens/add_product.dart
@@ -179,22 +179,22 @@ class _AddProductState extends State<AddProduct> {
 
               Row(
                 children: <Widget>[
-                  Checkbox(value: selectedSizes.contains('XS'), onChanged: changeSelectedSize),
+                  Checkbox(value: selectedSizes.contains('XS'), onChanged: (value)=>changeSelectedSize('XS')),
                   Text('XS'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('S'), onChanged: (value)=>changeSelectedSize('S')),
                   Text('S'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('M'), onChanged: (value)=>changeSelectedSize('M')),
                   Text('M'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('L'), onChanged: (value)=>changeSelectedSize('L')),
                   Text('L'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('XL'), onChanged: (value)=>changeSelectedSize('XL')),
                   Text('XL'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('XXL'), onChanged: (value)=>changeSelectedSize('XXL')),
                   Text('XXL'),
 
                 ],
@@ -203,43 +203,43 @@ class _AddProductState extends State<AddProduct> {
               Text('Available sizes'),
               Row(
                 children: <Widget>[
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('28'), onChanged: (value)=>changeSelectedSize('28')),
                   Text('28'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('30'), onChanged: (value)=>changeSelectedSize('32')),
                   Text('30'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('32'), onChanged: (value)=>changeSelectedSize('32')),
                   Text('32'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('34'), onChanged: (value)=>changeSelectedSize('34')),
                   Text('34'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('36'), onChanged: (value)=>changeSelectedSize('36')),
                   Text('36'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('38'), onChanged: (value)=>changeSelectedSize('38')),
                   Text('38'),
                 ],
               ),
               Row(
                 children: <Widget>[
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('40'), onChanged: (value)=>changeSelectedSize('40')),
                   Text('40'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('42'), onChanged: (value)=>changeSelectedSize('42')),
                   Text('42'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('44'), onChanged: (value)=>changeSelectedSize('44')),
                   Text('44'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('46'), onChanged: (value)=>changeSelectedSize('46')),
                   Text('46'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('48'), onChanged: (value)=>changeSelectedSize('48')),
                   Text('48'),
 
-                  Checkbox(value: false, onChanged: null),
+                  Checkbox(value: selectedSizes.contains('50'), onChanged: (value)=>changeSelectedSize('50')),
                   Text('50'),
                 ],
               ),
@@ -288,8 +288,17 @@ class _AddProductState extends State<AddProduct> {
     setState(()=>_currentBrand=selectedbrand);
   }
 
-  void changeSelectedSize(bool size) {
+  void changeSelectedSize(String size) {
+    if(selectedSizes.contains(size)){
+      setState(() {
+        selectedSizes.remove(size);
+      });
+    }else{
+      setState(() {
+        selectedSizes.insert(0,size);
+      });
 
+    }
   }
 }
 
